5254089def86c7311e5ad652db1280597b4add88
AtomicLemon <46167928+AtomicLemon@users.noreply.github.com>
2019-10-20 18:28:14 +1300

    No arguments for !ping

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 	dg.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate, args string) {
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
