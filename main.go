f990a1b18c71e8337a0f2a982a2999a784760d4e
AtomicLemon <46167928+AtomicLemon@users.noreply.github.com>
2019-10-20 17:08:35 +1300

    Create main.go

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error creating a Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate, args string) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "!ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
